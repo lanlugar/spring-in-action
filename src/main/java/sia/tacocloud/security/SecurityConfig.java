cf8817bf4333559f98e6b9ff4cb80bca9621299d
Lan Lugar <lanlugar@gmail.com>
2023-10-24 03:33:02 +0200

    in memory UserService implemented with few users

--- src/main/java/sia/tacocloud/security/SecurityConfig.java
+++ src/main/java/sia/tacocloud/security/SecurityConfig.java
@@ -1,15 +1,40 @@
 package sia.tacocloud.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-@Component
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
 public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        List<UserDetails> usersList = new ArrayList<>();
+        usersList.add(User
+                .withUsername("buzz")
+                .password(encoder.encode("password"))
+                .authorities(new SimpleGrantedAuthority("ROLE_USER"))
+                .build());
+        usersList.add(User
+                .withUsername("woody")
+                .password(encoder.encode("password"))
+                .authorities(new SimpleGrantedAuthority("ROLE_USER"))
+                .build());
+
+        return new InMemoryUserDetailsManager(usersList);
     }
 }
