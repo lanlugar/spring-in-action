2ba52b11e963cbdd6b1482613fab46753df14c0c
Lan Lugar <lanlugar@gmail.com>
2023-11-02 14:49:52 +0100

    path protected

--- src/main/java/sia/tacocloud/security/SecurityConfig.java
+++ src/main/java/sia/tacocloud/security/SecurityConfig.java
@@ -2,10 +2,12 @@ package sia.tacocloud.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
@@ -24,5 +26,19 @@ public class SecurityConfig {
 
             throw new UsernameNotFoundException("User '" + username + "' not found");
         };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeRequests()
+                .antMatchers("/design", "/orders").access("hasRole('USER')")
+                .antMatchers("/", "/**").access("permitAll()")
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/design")
+                .and()
+                .build();
     }
 }
--- src/main/java/sia/tacocloud/web/WebConfig.java
+++ src/main/java/sia/tacocloud/web/WebConfig.java
@@ -1,16 +1,18 @@
-//package sia.tacocloud.web;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Slf4j
-//public class WebConfig  implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addViewControllers(ViewControllerRegistry registry) {
-//        log.info("Requesting for homepage");
-//        registry.addViewController("/").setViewName("homePage");
-//    }
-//}
+package sia.tacocloud.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Slf4j
+@Configuration
+public class WebConfig  implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        log.info("Requesting for homepage");
+        registry.addViewController("/").setViewName("homePage");
+        registry.addViewController("/login").setViewName("loginPage");
+    }
+}
